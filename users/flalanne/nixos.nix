7e9d0787d7e8212d2f504bc3c635c2b798ad2a87
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-09 15:51:10 -0300

    Fix user configuration

--- users/flalanne/nixos.nix
+++ users/flalanne/nixos.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 {
-  users.users.mitchellh = {
+  users.users.flalanne = {
     isNormalUser = true;
-    home = "/home/pipex";
+    home = "/home/flalanne";
     shell = pkgs.zsh;
-    extraGroups = [ "docker" "wheel" ];
+    extraGroups = [ "users" "docker" "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCSqTJlGCR0O65PUwCkJDK3+0VFUdH2OKz2NosAPrq1 flalanne" ];
   };
 
