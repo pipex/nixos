1cb21bc9b7df08677d95c0d2aa56007c82f82b93
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-09 15:45:00 -0300

    Update authorized keys

--- users/flalanne/nixos.nix
+++ users/flalanne/nixos.nix
@@ -6,7 +6,7 @@
     home = "/home/pipex";
     shell = pkgs.zsh;
     extraGroups = [ "docker" "wheel" ];
-    openssh.authorizedKeys.keyFiles = [ (builtins.fetchurl https://github.com/pipex.keys) ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCSqTJlGCR0O65PUwCkJDK3+0VFUdH2OKz2NosAPrq1 flalanne" ];
   };
 
   services.mingetty.autologinUser = "flalanne";
