a04ca3cfed029d1169e0089d21ad8f16fc1504a0
Felipe Lalanne <1822826+pipex@users.noreply.github.com>
2022-11-09 16:45:21 -0300

    Enable getty

--- system/shared.nix
+++ system/shared.nix
@@ -4,10 +4,11 @@
   # Be careful updating this.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.kernelParams = [
-    "console=ttyS0,115200"
-    "console=tty1"
-  ];
+  systemd.services."serial-getty@ttyS0" = {
+    enable = true;
+    wantedBy = [ "getty.target" ]; # to start at boot
+    serviceConfig.Restart = "always"; # restart when session is closed
+  };
 
   nix = {
     # use unstable nix so we can access flakes
